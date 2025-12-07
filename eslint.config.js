3aaf434104675e07b089d75e36f29c6d5914cc44
magiclz233 <magiclz233@163.com>
2025-12-07 14:35:31 +0800

    feat: 添加 TypeScript 支持及相关配置
    - 新增 tsconfig.json 配置文件
    - 添加 typescript 和 typescript-eslint 依赖
    - 更新 eslint 配置以支持 TypeScript
    - 在 package.json 中添加 typecheck 脚本

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,22 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+  ...tseslint.configs.recommended,
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      parser: tseslint.parser,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
